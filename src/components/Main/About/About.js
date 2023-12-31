f0b50ac172bf57994cfd5942f83884d80352f35d
HarshilKhimasia <harshilkhimasia22@gmail.com>
2023-12-31 20:32:57 +0530

    First Commit

--- src/components/Main/About/About.js
+++ src/components/Main/About/About.js
@@ -1,4 +1,21 @@
 function About() {
-  return <h1>Hello World</h1>;
+  return (
+    <div className="aboutContainer">
+      <div className="aboutRow">
+        <div className="aboutCol"></div>
+        <div className="aboutCol">
+          <div>
+            <h2>About Me</h2>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quod
+              consectetur suscipit, officiis maxime voluptas sunt inventore
+              distinctio molestias nobis voluptates, exercitationem voluptatibus
+              quam accusamus asperiores corporis fugit error qui. Aut!
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 export default About;
